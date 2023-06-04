ed2c20caac0960dd0e8676d8a8a4d2f8314461d4
Vladyslav Melnychenko <webmonch@gmail.com>
2023-06-04 19:59:17 +0100

    upd

---
+++ pages/api/callStatusHandler.tsx
@@ -0,0 +1,42 @@
+import { NextApiHandler } from 'next'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
+import { PrismaClient } from '@prisma/client'
+import {validateRequest, twiml, Twilio} from 'twilio';
+
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+
+const ProtectedRoute: NextApiHandler = async (req, res) => {
+    const prisma = new PrismaClient()
+
+    console.log('Incoming fordwarder webhook')
+
+    
+
+    const twilioSignature = req.headers['x-twilio-signature'];
+    const url = 'https://upwork-callback-bot.vercel.app/api/callStatusHandler'
+
+    const isValidRequest = validateRequest(
+        authToken!,
+        //@ts-ignore
+        twilioSignature,
+        url,
+        req.body
+    );
+
+    if (!isValidRequest){
+        console.error('Not valid request signature')
+        return res.status(400).end()
+    }
+
+    const tw = new Twilio(accountSid, authToken);
+
+    
+
+    
+    return res.status(200)
+}
+
+export default ProtectedRoute
