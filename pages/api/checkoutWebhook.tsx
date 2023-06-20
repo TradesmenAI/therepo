a0b99c353afe5278117857b1d4782307e3a82bf7
Vladyslav Melnychenko <webmonch@gmail.com>
2023-06-20 15:38:06 +0100

    add twilio logs

--- pages/api/checkoutWebhook.tsx
+++ pages/api/checkoutWebhook.tsx
@@ -112,6 +112,7 @@ const WebhookRoute: NextApiHandler = async (req, res) => {
             let twilio_number = userProfile.twilio_number
 
             if (!twilio_number){
+                console.log('CREATING TWILIO NUMBER')
                 const client = new Twilio(accountSid, authToken);
                 const availableNumbers = await client.availablePhoneNumbers('GB').local.list({ smsEnabled: true })
             
